72dffd0502635f5e67cce83d9b860a3a764b7c49
WillianGust <williandes.silva@yahoo.ca>
2022-10-08 16:13:42 -0400

    Comecando condicionais

--- src/main/java/br/com/willian/App.java
+++ src/main/java/br/com/willian/App.java
@@ -4,22 +4,107 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.print.attribute.IntegerSyntax;
+
 public class App 
 {
-    public static void main( String[] args )
-    throws IOException
+    public static void main( String[] args ) throws IOException
     {
 
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(System.in));
-            System.out.println("Digite um numero x: ");
-            int x = Integer.parseInt(reader.readLine());
-            System.out.println("Digite um numero y: ");
-            int y = Integer.parseInt(reader.readLine());
+      // Dia 5 - Tipo primitivos variaveis
+
+        // double restoDivisao = 3.5/2;
+
+        // System.out.println("O resto da divisao e: " + restoDivisao);
+
+        // var hojeEstaVentando = false;
+        // System.out.println("Esta frio hoje? " + hojeEstaVentando);
+        
+        // var moeda = 4.5;
+        // var inteiro = 4;
+        // var stringNumero = "8";
+
+        // var valor = moeda * inteiro;
+        // var valor2 = moeda - inteiro;
+
+        
+        // Conversao String para numero CASTING da soma ficar correta 
+        // var valor3 = moeda + inteiro + Double.parseDouble(stringNumero);
+        // System.out.println("Resultado " + valor3);
+        
+        // var rotulo = "Nome: ";
+        // var nome = "Willian";
+        // var resultado = String.format("O valor de nome e: %s e o valor de rotulo e : %s", nome, rotulo);
+        // System.out.println(resultado);
+
+        // var calculo = 4.5555;
+        // System.out.printf("O valor e %.2f", calculo); // Mostra numeros com duas casas decimais
+
+        /* EXERCICIO
+         *  Joao tem um armazem de um programa qua solicite
+         * o nome de seu cliente
+         * o endereco do cliente
+         * o nome do material de cosntrucao
+         * o valor do material de construcao
+         * a quantidade que o cliente deseja
+         * no final do programa, mostrar um relatorio com o valor total do produto comprado
+         * 
+         * RELATORIO
+         * 
+         * --------------------
+         * Cliente: Joao
+         * Endereco: Rua ...
+         * Comprou o material: XXXXno valor de XXX
+         * O valor total da compra foi : R& XXXX
+         * -------------------
+         */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("==============[ CALCULO PRODUTO/CLIENTE]==============");
+        // System.out.println("Ola Joao seja muito bem vindo, comecemos?");
+        System.out.println("Digite o nome do seu cliente ");
+        var nome = reader.readLine();
+
+        System.out.println("Digite o endereco do seu cliente ");
+        var endereco = reader.readLine();
+
+        System.out.println("Digite o nome do produto ");
+        var produto = reader.readLine();
+
+        System.out.printf("Digite o valor do produto (%s):", produto);
+        var valor = Double.parseDouble(reader.readLine());
+
+        System.out.printf("Digite a quantidade do produto (%s):", produto);
+        var quantidade = Integer.parseInt(reader.readLine());
+
+        var valorTotal = valor * quantidade;
+       
+       
+        System.out.println("----------------------------------");
+
+        System.out.printf("Cliente: %s \n", nome);
+        System.out.printf("Endereco: %s \n", endereco);
+        System.out.printf("Comprou o material: %s valor de R$ %s \n", produto, valor);
+        System.out.printf("O valor total da compra foi : R$ %.2f \n", valorTotal);
+        if(valorTotal > 100){
+          System.out.printf("O pedido foi maior que R$ 100,00, voce tera um desconto em sua proxima compra conosco \n");
+        }
+
+        System.out.println("----------------------------------");
+
+         
+
+        // Exercicio dia 4
+        // BufferedReader reader = new BufferedReader(
+        //     new InputStreamReader(System.in));
+        //     System.out.println("Digite um numero x: ");
+        //     int x = Integer.parseInt(reader.readLine());
+        //     System.out.println("Digite um numero y: ");
+        //     int y = Integer.parseInt(reader.readLine());
             
-            var resultado = (x * 8 + y) - x + y / 100;
-            System.out.println("(" + x + " * 8 - " + y + ") - " + x + " + " + y + " / 100");
-            System.out.println("O resultado da equacao e de : " + resultado);
+        //     var resultado = (x * 8 + y) - x + y / 100;
+        //     System.out.println("(" + x + " * 8 - " + y + ") - " + x + " + " + y + " / 100");
+        //     System.out.println("O resultado da equacao e de : " + resultado);
 
             // System.out.println("Digite um numero: ");
             // int numero = Integer.parseInt(reader.readLine());
